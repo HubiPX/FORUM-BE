882efdc238807362f23328a6d6c23731e32199fc
HubiPX <hubert.bercik@o2.pl>
2023-10-16 13:10:05 +0200

    place in index

--- posts/posts.py
+++ posts/posts.py
@@ -123,4 +123,4 @@ def quantity(user_id):
     postssug = db.session.query(Postssug).count()
     postsbugs = db.session.query(Postsbugs).count()
     return {"postsa": postsa, "postsm": postsm, "postsv": postsv, "postsnews": postsnews,
-            "postssug": postssug, "postsbugs": postsbugs, "place": place}
+            "postssug": postssug, "postsbugs": postsbugs, "placeId": place}
