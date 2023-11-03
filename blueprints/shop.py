3c307c4c85eea4d95db0cc28bdccbdcb115a28eb
HubiPX <hubert.bercik@o2.pl>
2023-11-03 15:53:00 +0100

    price - backend

--- blueprints/shop.py
+++ blueprints/shop.py
@@ -16,7 +16,6 @@ def _buy_(user_id):
         return '', 404
 
     post = request.get_json()
-
     option = int(post.get("option"))
     time = int(post.get("time"))
 
@@ -27,7 +26,7 @@ def _buy_(user_id):
 
     prices = {
         1: {'7dni': 0, '30dni': 0, '180dni': 0},
-        2: {'7dni': 0, '30dni': 0, '180dni': 0},
+        2: {'7dni': 1000, '30dni': 4000, '180dni': 10000},  # dni odpowiadają kolejno 1K 4K i 10K exp
         3: {'7dni': 300, '30dni': 1500, '180dni': 5000},
         4: {'7dni': 400, '30dni': 1750, '180dni': 6000},
         5: {'7dni': 500, '30dni': 2000, '180dni': 7500},
@@ -42,11 +41,11 @@ def _buy_(user_id):
     price_option = prices[option]
 
     if time == 7:
-        score = -price_option['7dni']
+        score = price_option['7dni']
     elif time == 30:
-        score = -price_option['30dni']
+        score = price_option['30dni']
     elif time == 180:
-        score = -price_option['180dni']
+        score = price_option['180dni']
     else:
         return '', 400
 
@@ -63,13 +62,35 @@ def _buy_(user_id):
     elif 3 <= option <= 5 and color_nick == 0 and x >= -score:
         user.color_nick = option - 2
         user.cnick_date = today + datetime.timedelta(days=time)
-        user.score = x + score
+        user.score = x - score
     elif 6 <= option <= 8 and rank == 0 and x >= -score:
         user.rank = option - 5
         user.rank_date = today + datetime.timedelta(days=time)
-        user.score = x + score
+        user.score = x - score
     else:
         return '', 400
+
+    db.session.commit()
+    return '', 200
+
+
+@shop.route("/<user_id>/del", methods=['post'])
+@Auth.logged_user
+def _del_(user_id):
+    user = Users.query.filter_by(id=user_id).first()
+
+    if not Users.query.filter_by(id=user_id).first():
+        return '', 404
+
+    post = request.get_json()
+    option = int(post.get("option"))
+
+    if option == 1:
+        user.color_nick = 0
+        user.cnick_date = None
+    elif option == 2:
+        user.rank = 0
+        user.rank_date = None
 
     db.session.commit()
     return '', 200
