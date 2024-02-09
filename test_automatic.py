3132e7c2d9efdccaf90d7bbddaf4d2d015acaa49
HubiPX <hubert.bercik@o2.pl>
2024-02-09 18:18:42 +0100

    tests automatic

--- test_automatic.py
+++ test_automatic.py
@@ -32,13 +32,13 @@ class TestUserBlueprint(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json["username"], "admin")
         self.assertEqual(response.json["is_admin"], 4)
-        logger.info('Test - poprawne logowanie ADMIN.')
+        # logger.info('Test - poprawne logowanie ADMIN.')
         return 0
 
     def admin_logout(self):
         response = self.client.get('/api/logout')
         self.assertEqual(response.status_code, 200)
-        logger.info('Test - poprawne wylogowanie ADMIN.')
+        # logger.info('Test - poprawne wylogowanie ADMIN.')
         return 0
 
     def create_user(self):
@@ -51,7 +51,7 @@ class TestUserBlueprint(TestCase):
         response = self.client.post('/api/users/create', json=data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data.decode('utf-8'), 'Utworzono konto pomyślnie!')
-        logger.info('Test - poprawne tworzenia konta.')
+        # logger.info('Test - poprawne tworzenia konta.')
         return 0
 
     def login_user(self):
@@ -65,13 +65,13 @@ class TestUserBlueprint(TestCase):
         self.assertEqual(response.json["username"], "test_user")
         self.assertEqual(response.json["is_admin"], 0)
         user_id = response.json["user_id"]
-        logger.info('Test - poprawne logowanie na test_user.')
+        # logger.info('Test - poprawne logowanie na test_user.')
         return user_id
 
     def logout_user(self):
         response = self.client.get('/api/logout')
         self.assertEqual(response.status_code, 200)
-        logger.info('Test - poprawne wylogowanie test_user.')
+        # logger.info('Test - poprawne wylogowanie test_user.')
         return 0
 
     def delete_user(self, user_id):
@@ -81,7 +81,7 @@ class TestUserBlueprint(TestCase):
 
         response = self.client.post(f'api/admin/{user_id}/delete', json=data)
         self.assertEqual(response.status_code, 200)
-        logger.info('Test - poprawne usunięcie test_user.')
+        # logger.info('Test - poprawne usunięcie test_user.')
 
     def user_set_admin_lvl(self, user_id, admin_lvl):
         data = {
@@ -89,19 +89,20 @@ class TestUserBlueprint(TestCase):
         }
         response = self.client.post(f'api/admin/{user_id}/lvl-admin', json=data)
         self.assertEqual(response.status_code, 200)
-        logger.info(f'Test - poprawne ustawienie admin-lvl na {admin_lvl} dla test_user.')
+        # logger.info(f'Test - poprawne ustawienie admin-lvl na {admin_lvl} dla test_user.')
 
     #####################################################################
     def test_admin_login_and_logout(self):
         self.admin_login()
         self.admin_logout()
+        logger.info('Test 1 - logowanie i wylogowanie na ADMINa.')
 
     def test_admin_show_users(self):
         self.admin_login()
 
         response = self.client.get('/api/admin')
         self.assertEqual(response.status_code, 200)
-        logger.info('Test - poprawne pobranie listy użytkowników.')
+        logger.info('Test 2 - poprawne pobranie listy użytkowników przez ADMINa.')
 
         self.admin_logout()
 
@@ -113,6 +114,7 @@ class TestUserBlueprint(TestCase):
 
         self.delete_user(user_id)
         self.admin_logout()
+        logger.info('Test 3 - poprawne tworzenie konta użytkownika.')
 
     def test_create_user_with_different_passwords(self):
         data = {
@@ -124,7 +126,7 @@ class TestUserBlueprint(TestCase):
         response = self.client.post('/api/users/create', json=data)
         self.assertEqual(response.status_code, 406)
         self.assertEqual(response.data.decode('utf-8'), 'Podane hasła są różne!')
-        logger.info('Test - niepoprawne tworzenia konta z różnymi hasłami.')
+        logger.info('Test 4 - niepoprawne tworzenia konta z różnymi hasłami.')
 
     def test_change_user(self):
         self.create_user()
@@ -134,17 +136,17 @@ class TestUserBlueprint(TestCase):
 
         for i in range(4):
             self.user_set_admin_lvl(user_id, i)
-
+        logger.info('Test 5 - poprawne ustawienie admin-lvl od 0 do 3 dla test_user.')
         response = self.client.get(f'/api/admin/{user_id}/reset-password')
         self.assertEqual(response.status_code, 200)
-        logger.info('Test - poprawna zmiana hasła dla test_user.')
+        logger.info('Test 5 - poprawna zmiana hasła dla test_user.')
 
         data = {
             "new_score": "1000"
         }
         response = self.client.post(f'api/admin/{user_id}/set-score', json=data)
         self.assertEqual(response.status_code, 200)
-        logger.info('Test - poprawna zmiana ilości expa test_user.')
+        logger.info('Test 5 - poprawna zmiana ilości expa test_user.')
 
         self.delete_user(user_id)
         self.admin_logout()
@@ -161,7 +163,7 @@ class TestUserBlueprint(TestCase):
 
         response = self.client.post(f'api/users/change-password', json=data)
         self.assertEqual(response.status_code, 200)
-        logger.info('Test - poprawna zmiana hasła.')
+        logger.info('Test 6 - poprawna zmiana hasła.')
 
         self.admin_login()
         self.delete_user(user_id)
